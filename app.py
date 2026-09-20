4716cf1a084c5fd806ca43cc2ff5af4617a24984
Thiri-Mon <thinthirimon97@gmail.com>
2026-09-20 03:43:38 -0700

    Admin Panel: Relocate database reset switch completely out of active uploader conditional scope

--- app.py
+++ app.py
@@ -513,14 +513,7 @@ if st.sidebar.button("Store into Database"):
             else:
                 st.sidebar.error("❌ Thesis data not found!")
 
-    st.sidebar.markdown("---")
-    if st.sidebar.button("🚨 WIPE ALL DATABASE TITLES"):
-        try:
-            collection.delete(where={})
-            st.sidebar.success("💥 Database fully cleared back to 0!")
-            st.rerun()
-        except Exception as wipe_fault:
-            st.sidebar.error(f"Failed to clear database index: {str(wipe_fault)}")
+    
     else:
         st.sidebar.warning("⚠️ Firstly,please upload the file.")
 # If ChromaDB already exists
@@ -532,6 +525,15 @@ if not is_database_empty():
         st.sidebar.metric(label="Total number of Titles", value=total_records)
     except Exception as e:
         pass
+
+st.sidebar.markdown("---")
+    if st.sidebar.button("🚨 WIPE ALL DATABASE TITLES"):
+        try:
+            collection.delete(where={})
+            st.sidebar.success("💥 Database fully cleared back to 0!")
+            st.rerun()
+        except Exception as wipe_fault:
+            st.sidebar.error(f"Failed to clear database index: {str(wipe_fault)}")
 
 # Clear Chat History
 if st.sidebar.button("🗑️ Clear Chat History"):
